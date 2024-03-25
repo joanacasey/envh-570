580e1ece50337606873c1411dfa6703bde86aea0
Brian High <high@uw.edu>
2024-03-25 20:19:12 +0000

    Minor bug fixes and stylistic updates.

--- code/lab/04/lab04_envh570.R
+++ code/lab/04/lab04_envh570.R
@@ -17,18 +17,24 @@ pacman::p_load(
   leaflegend,
   ggmap,
   raster,
-  terra,
-  rgdal
+  terra
+  # rgdal   # Note: This package has been deprecated, see below for work-around.
 )
 
-##############################################################################################################
-#PART 1#
+# Since rgdal is deprecated we need to install the last version in the archive.
+# See: https://cran.r-project.org/web/packages/rgdal/index.html
+# And: https://stackoverflow.com/questions/76868135/
+pacman::p_install_version("rgdal", version = "1.6-7")
+pacman::p_load(rgdal)
+
+##############################################################################
+# PART 1 
 # Bring in Seattle data with SES and trees
 # What is the projection?
-seattle <- st_read("data/lab/04/seattle_tree_ses_ct.shp")
+seattle <- st_read(here("data/lab/04/seattle_tree_ses_ct.shp"))
 glimpse(seattle)
 
-#Plot tree density
+# Plot tree density
 tree_density <- ggplot() +
   geom_sf(data = seattle, aes(fill = tr_dnst)) +
   theme_void(base_size = 14) +
@@ -43,7 +49,7 @@ tree_density <- ggplot() +
                        na.value = "grey50")
 
 
-#Plot median income
+# Plot median income
 income <- ggplot() +
   geom_sf(data = seattle, aes(fill = MEDIAN_)) +
   theme_void(base_size = 14) +
@@ -59,7 +65,7 @@ income <- ggplot() +
 
 tree_density + income
 
-#Try adding figure for percent vacant housing below
+# Try adding figure for percent vacant housing below
 ggplot() +
   geom_sf(data = seattle, aes(fill = PCT_VAC)) +
   theme_void(base_size = 14) +
@@ -72,10 +78,11 @@ ggplot() +
                            colors = met.brewer("Degas"),
                            na.value = "grey50")
 
-##############################################################################################################
+#############################################################################
 ## PART 2: CROWD-SOURCED NOISE MEASUREMENTS
 noise <-
-  read_csv("data/lab/04/noise_envh570_seattle.csv") #cool that R recognizes the time column as time of day
+  read_csv(here("data/lab/04/noise_envh570_seattle.csv")) 
+# cool that R recognizes the time column as time of day
 
 #Remove erroneous rows of data
 noise <- drop_na(noise)
@@ -142,13 +149,13 @@ m2 <- leaflet() %>%
     breaks = 5)
 m2
 
-##############################################################################################################
+#############################################################################
 ## PART 3: CROWD-SOURCED NOISE MEASUREMENTS + SOCIAL ENVIRONMENT 
 
-#Change noise measurements into same projection as Seattle shapefile
+# Change noise measurements into same projection as Seattle shapefile
 noise_sf <- st_transform(noise_sf, crs = st_crs(seattle))
 
-#Map points on the tree map
+# Map points on the tree map
 ggplot() +
   geom_sf(data = seattle, aes(fill = tr_dnst)) +
   geom_sf(data = noise_sf, aes(size=noise), color="grey", alpha=0.5) +
@@ -164,38 +171,38 @@ ggplot() +
                        na.value = "grey50") +
   scale_size_continuous("Noise level (dB)")
 
-#Create a similar map with income 
+# Create a similar map with income 
 ##ADD ME##
 
-##############################################################################################################
+#############################################################################
 ## PART 4: CROWD-SOURCED NOISE MEASUREMENTS VS NATIONAL NOISE MODEL
 transit_noise <- raster("data/lab/04/seattle_transit.tif")
 
-#Check out specs of the raster data
+# Check out specs of the raster data
 transit_noise #How many columnns and rows of data? How many total grid cells?
 
-#Plot it
+# Plot it
 plot(transit_noise)
 
-#Extract transit noise levels where we took noise measurements
+# Extract transit noise levels where we took noise measurements
 noise_from_transit_model <- terra::extract(transit_noise, noise_sf)
 
-#Gives us values from from the transit noise raster
-#We need to add them back to the noise_sf shapefile
+# Gives us values from from the transit noise raster
+# We need to add them back to the noise_sf shapefile
 noise_sf <- noise_sf %>% mutate(transit_noise = noise_from_transit_model)
 
-#What is the correlation between our measurements and the model?
+# What is the correlation between our measurements and the model?
 ggplot(noise_sf, aes(noise, transit_noise)) + geom_point() + 
   geom_smooth() +
   theme_minimal() +
-  geom_abline(linetype="dashed") + #adding a reference line +
+  geom_abline(linetype="dashed") + # adding a reference line +
   scale_x_continuous("Our noise measurements (dBA)", limits=c(40,80))+
   scale_y_continuous("Transit noise model (dBA)", limits=c(40,80))
 
 
-#Pearson and Spearman correlation
+# Pearson and Spearman correlation
 cor.test(noise_sf$noise, noise_sf$transit_noise,
          method = "spearman")
 
-#You do Pearson 
+# You do Pearson 
 #ADD ME
