c95f4f005fc56d02a7033bb7a07358d04f871ab6
Brian High <high@uw.edu>
2024-03-28 22:49:42 +0000

    Added a comment to bash script.

--- code/lab/install_packages.sh
+++ code/lab/install_packages.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 time Rscript --vanilla --no-save code/lab/package_installer.R
+
+# The above is equivalent to running this command in R:
+# system.time(source("code/lab/package_installer.R"))
